556dc4c3a3a94d799881b52eb4e6f995e51adba1
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2024-11-01 17:51:55 +0100

    user too much on the right

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { UserButton, useUser } from "@clerk/nextjs";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
 import { faCartShopping, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
@@ -10,14 +10,14 @@ const Header = () => {
     const { user } = useUser();
 
     return ( 
-        <div className="w-screen h-[13vh] bg-gray-950 flex flex-row justify-start items-center sm:h-[10vh] md:h-[11vh] lg:h-[12vh] xl:h-[13vh]">
-            <Link href="/" className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold w-[20%] h-full flex justify-start items-center ml-[3%] sm:ml-[5%] md:ml-[10%] lg:ml-[15%]">
-                <h1 className="text-wrap sm:text-wrap md:text-wrap lg:text-nowrap xl:text-nowrap whitespace-normal">
+        <div className="w-screen h-[13vh] bg-gray-950 flex flex-row justify-start items-center ">
+            <Link href="/" className="text-white text-xl sm:text-[1.4rem] md:text-[1.6rem] lg:text-4xl font-semibold w-[20%] h-full flex justify-start items-center ml-[3%] sm:ml-[5%] md:ml-[10%] lg:ml-[15%]">
+                <h1 className="text-wrap sm:text-wrap md:text-wrap lg:text-nowrap xl:text-nowrap whitespace-normal ">
                     <span className="text-yellow-300">Turkey</span>Expert
                 </h1>
             </Link>
-
-            <div className="w-[40%] h-full flex justify-center items-center sm:w-[45%] md:w-[40%] lg:w-[35%] xl:w-[30%]">
+            
+            <div className="w-[40%] h-full justify-center items-center sm:w-[45%] md:w-[40%] lg:w-[35%] xl:w-[30%] sm:hidden md:flex lg:flex xl:flex 2xl:flex hidden">
                 <input 
                     type="text" 
                     className="w-[80%] min-h-14 h-14 rounded-l-xl p-4 placeholder:text-black text-black text-base sm:text-sm md:text-base lg:text-lg xl:text-xl focus:outline-none"
@@ -28,21 +28,19 @@ const Header = () => {
                 </button>
             </div>
 
-            <div className="w-[10%] h-full flex justify-evenly items-center sm:w-[12%] md:w-[10%] lg:w-[8%] xl:w-[6%]">
+            <div className="w-[40%] h-full sm:w-[45%] md:w-[40%] lg:w-[35%] xl:w-[30%] sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden flex"></div>
+
+
+            <div className="w-[40%] h-full flex justify-evenly items-center sm:w-[12%] md:w-[10%] lg:w-[8%] xl:w-[6%]">
                 <Link href="/favourites">
-                    <FontAwesomeIcon icon={faHeart} className="text-yellow-300 w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-7 lg:h-7 xl:w-8 xl:h-8"/>
+                    <FontAwesomeIcon icon={faHeart} className="text-yellow-300 w-8 h-8 "/>
                 </Link>
                 <Link href="/checkout">
-                    <FontAwesomeIcon icon={faCartShopping} className="text-yellow-300 w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-7 lg:h-7 xl:w-8 xl:h-8"/>
+                    <FontAwesomeIcon icon={faCartShopping} className="text-yellow-300 w-8 h-8 "/>
                 </Link>
-            </div>
+                <div className=" border-l border-gray-400  mx-2 h-0 sm:h-6 md:h-7 lg:h-8 xl:h-10"></div>
 
-            <div className="h-8 border-l border-gray-400 mx-4 sm:h-6 md:h-7 lg:h-8 xl:h-10"></div>
-
-            <div className="w-[30%] h-full justify-start items-center flex pr-[5%] sm:w-[25%] md:w-[20%] lg:w-[18%] xl:w-[15%]">
-                <button className="flex items-center space-x-2 w-[70%] sm:w-[60%] md:w-[70%] lg:w-[80%] xl:w-[70%]">
-                    <div>
-                        {user ? (
+                {user ? (
                             <UserButton
                                 appearance={{
                                     elements: {
@@ -58,13 +56,13 @@ const Header = () => {
                                 }}
                             />           
                         ) : (
-                            <Link href="/sign-in" className="text-yellow-300 hover:underline text-base sm:text-sm md:text-base lg:text-lg xl:text-xl">
-                                Sign In
+                            <Link href="/sign-in">
+                                <FontAwesomeIcon icon={faUser} className="text-yellow-300 w-8 h-8 "/>
                             </Link>
                         )}
-                    </div>
-                </button>
             </div>
+
+
         </div>
     );
 }
